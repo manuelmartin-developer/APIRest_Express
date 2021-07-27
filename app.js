3322eba07dd55cc5171bcd9ee846a7b6101839bf
Manuel Martin <hola@manuelmartin.name>
2021-07-27 15:43:18 +0200

    Finish project and chinpun

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = require('./routes/router');
+
+const app = express();
+const port = 3000;
+
+//Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Routes
+app.use('/api', router);
+
+// 404 - Page not found
+app.use(function(req, res, next) {
+    return res.status(404).send({ message: 'Pagina -' +req.url+'- Not found.' });
+  });
+  
+  // 500 - Any server error
+  app.use(function(err, req, res, next) {
+    return res.status(500).send({ error: err });
+  });
+
+app.listen (port, () => {
+    console.log(`Servidor Escuchando  http://localhost:${port}`);
+});
---
+++ utils/films.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const films = {
+
+getOneFilm: async (title) => {
+
+  const res = await fetch(`https://www.omdbapi.com/?t=${title}&apikey=${process.env.API_KEY}`);
+  const film = await res.json();
+  return film;
+
+}
+
+};
+
+module.exports = films;
